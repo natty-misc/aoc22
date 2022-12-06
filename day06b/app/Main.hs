f714dbdcdd832bf1867ff87846beec038df995f2
Natty <31473117+AMNatty@users.noreply.github.com>
2022-12-06 09:43:48 +0100

    Wait why am I pattern matching booleans

--- day06b/app/Main.hs
+++ day06b/app/Main.hs
@@ -5,16 +5,12 @@ import qualified Data.Set as S
 takeExact :: Int -> [a] -> Either String [a]
 takeExact amt arr = do
     let h = take amt arr
-    case amt == length h of
-        True -> Right h
-        False -> Left "Length error"
+    if amt == length h then Right h else Left "Length error"
 
 packetStart :: String -> Either String Int
 packetStart t  = do
     head <- takeExact 14 t
-    case (length head) == (length . S.fromList $ head) of
-        True -> Right 14
-        False -> (+1) <$> (packetStart (drop 1 t))
+    if (length head) == (length . S.fromList $ head) then Right 14 else (+1) <$> (packetStart (drop 1 t))
 
 main :: IO ()
 main = do
