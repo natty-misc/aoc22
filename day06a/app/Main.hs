3b9fb2bcd19c792789bf98a3177398abeda5f74f
Natty <31473117+AMNatty@users.noreply.github.com>
2022-12-06 09:27:08 +0100

    Lazy day 6 solution

---
+++ day06a/app/Main.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import qualified Data.Set as S
+
+takeExact :: Int -> [a] -> Either String [a]
+takeExact amt arr = do
+    let h = take amt arr
+    case amt == length h of
+        True -> Right h
+        False -> Left "Length error"
+
+packetStart :: String -> Either String Int
+packetStart t  = do
+    head <- takeExact 4 t
+    case (length head) == (length . S.fromList $ head) of
+        True -> Right 4
+        False -> (+1) <$> (packetStart (drop 1 t))
+
+main :: IO ()
+main = do
+    input <- getContents 
+    print . packetStart $ input
